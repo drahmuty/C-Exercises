48fd9cdb1ac45858042fe740f5c5fa66431d7333
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-13 21:39:50 -0500

    Update 1-8.c

--- 1-8.c
+++ 1-8.c
@@ -7,13 +7,16 @@ int main(void)
     int t = 0;
     int nl = 0;
     
-    while ((c = getchar()) != EOF)
+    while ((c = getchar()) != '0')      // Couldn't get EOF to work, so I replaced it with '0'
+    {
         if (c == ' ')
             ++b;
         if (c == '\t')
             ++t;
         if (c == '\n')
             ++nl;
+    }
+
     printf("Blanks: %d\n", b);
     printf("Tabs: %d\n", t);
     printf("Newlines: %d\n", nl);

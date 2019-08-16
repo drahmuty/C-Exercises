f7a424de88a5522cf2c27688cba3d5bc9dc650f1
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-15 20:46:51 -0500

    Update 1-12.c

--- 1-12.c
+++ 1-12.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main(void)
 {
+    int c, nl, nw, nc, state;
     
+    state = OUT;
+    nl = nw = nc = 0;
+    
+    while ((c = getchar()) != 0)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT)
+        {
+            state = IN;
+            putchar('\n');
+            putchar(c);
+        }
+    }
 }

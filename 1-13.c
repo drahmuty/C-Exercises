21b410d0a5fc80f88b3083d1d635b8adf203da56
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-19 20:09:17 -0500

    Update 1-13.c

--- 1-13.c
+++ 1-13.c
@@ -7,13 +7,17 @@ int main(void)
 {
     int c, len, state;
     
+    len = 0;
+    
     while ((c = getchar()) != 0)
     {
         if (c == ' ' || c == '\n' || c == '\t')
         {
             if (state == IN)
+            {
                 printf("%d", len);
                 putchar('\n');
+            }
             
             state = OUT;
             len = 0;

5783a037aa7ef3cc21dbe76036a69ffbed8aa9b2
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-19 19:49:25 -0500

    Update 1-13.c

--- 1-13.c
+++ 1-13.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main(void)
 {
+    int c, len, state;
     
+    while ((c = getchar()) != 0)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            if (state == IN)
+                printf("%d", len);
+                putchar('\n');
+            
+            state = OUT;
+            len = 0;
+        }
+        else 
+        {
+            if (state == OUT)
+                state = IN;
+            
+            ++len;
+        }
+    }
 }

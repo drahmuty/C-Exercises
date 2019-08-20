19cb28d2f2c8a42684bad5d649dfd12bd902ea48
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-19 21:11:15 -0500

    Update 1-13b.c

--- 1-13b.c
+++ 1-13b.c
@@ -1,3 +1,46 @@
 #include <stdio.h>
 
-int main(void) {}
+#define IN 1
+#define OUT 0
+#define MAX 10
+
+int main(void) 
+{
+    int c, i, j, state, len;
+    int words[MAX];
+    
+    state = OUT;
+    len = 0;
+    
+    for (i = 0; i < MAX; i++)
+        words[i] = 0;
+    
+    while ((c = getchar()) != '0')
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            if (state == IN)
+                ++words[len-1];
+            
+            state = OUT;
+            len = 0;
+        }
+        else 
+        {
+            if (state == OUT)
+                state = IN;
+            
+            ++len;
+        }
+    }
+//     for (i = 0; i < MAX; i++)
+//         printf("%d:\t%d\n", i+1, words[i]);      // Option to print numerial values
+    
+    for (i = 0; i < MAX; i++)
+    {
+        printf("%d:\t", i+1);
+        for (j = 0; j < words[i]; j++)
+            printf("*");
+        printf("\n");
+    }
+}

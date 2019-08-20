4209f6cdb5bcbe83c735e76edfdee3e4f3e512c1
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-19 22:15:52 -0500

    Update 1-14.c

--- 1-14.c
+++ 1-14.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
-#define MAX 4
+#define MIN 97
+#define MAX 123
 
 int main(void)
 {
-    int c, i;
-    int char_val[MAX];
-    int char_num[MAX];
+    int c, i, j;
+    int LEN = MAX - MIN;
+        
+    int char_val[LEN];
+    int char_num[LEN];
     
-    char_val[0] = 'a';
-    char_val[1] = 'b';
-    char_val[2] = 'c';
-    char_val[3] = 'x';
+    for (i = 0; i < LEN; i++)
+    {
+        char_val[i] = MIN + i;
+    }
     
-    for (i = 0; i < MAX; i++)
+    for (i = 0; i < LEN; i++)
         char_num[i] = 0;
     
     while ((c = getchar()) != '0')
     {
-        if (c == char_val[0])
-            ++char_num[0];
-        else if (c == char_val[1])
-            ++char_num[1];
-        else if (c == char_val[2])
-            ++char_num[2];
-        else
-            ++char_num[3];
+        for (i = 0; i < LEN; i++)
+            if (c == char_val[i])
+                ++char_num[i];
     }
-    for (i = 0; i < MAX; i++)
+    for (i = 0; i < LEN; i++)
     {
-        printf("%d\t%d\n", char_val[i], char_num[i]);
+        printf("%d\t", char_val[i]);
+//         printf("%d\t%d\n", char_val[i], char_num[i]);        // Option to show numerical values
+        
+        for (j = 0; j < char_num[i]; j++)
+            printf("*");
+        printf("\n");
     }
 }

c04c9eed95e28f5da7a17cc5f47c3d0ec4f75b9c
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-20 20:50:26 -0500

    Update 1-16.c

--- 1-16.c
+++ 1-16.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#define MAXLINE 1000
 
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(void)
+{
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+    
+    max = 10;
+    while ((len = get_line(line, MAXLINE)) > 0)
+        if (len > max) {
+            copy(longest, line);
+            printf("Exceeded max limit by %d\n", (len-max));
+            printf("%s\n", longest);
+        }
+    return 0;
+}
+
+int get_line(char s[], int lim)
+{
+    int c, i;
+    
+    for (i=0; i<lim-1 && (c=getchar())!='0' && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[])
+{
+    int i;
+    
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}

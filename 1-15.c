3c3fbd370607148956e6de4bfa9ba2931fe0f0f9
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-20 19:07:03 -0500

    Update 1-15.c

--- 1-15.c
+++ 1-15.c
@@ -1,4 +1,39 @@
 #include <stdio.h>
 
+int f_to_c(int);
+int c_to_f(int);
+
 int main(void)
-{}
+{
+    int i, min, max, inc;
+    
+    // Compute Fahrenheit to Celsius
+    min = 0;
+    max = 200;
+    inc = 20;
+    
+    printf("F\tC\n");
+    
+    for (i = min; i <= max; i = i + inc)
+        printf("%d\t%d\n", i, f_to_c(i));
+    
+    // Compute Celsius to Fahrenheit
+    min = 0;
+    max = 100;
+    inc = 10;
+    
+    printf("\nC\tF\n");
+    
+    for (i = min; i <= max; i = i + inc)
+        printf("%d\t%d\n", i, c_to_f(i));
+}
+
+int f_to_c(x)
+{
+    return (5.0 / 9.0) * (x - 32);
+}
+
+int c_to_f(x)
+{
+    return (9.0 / 5.0) * x + 32;
+}

9208c4da6f09ee63ac3b2562d3f9ad7ea74cfcaa
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-20 21:19:13 -0500

    Update 1-17.c

--- 1-17.c
+++ 1-17.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+#define MAXLINE 1000
+#define MAXITEMS 100
+#define THRESHOLD 10
+
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(void)
+{
+    int i, j;
+    int len;
+    
+    int item[MAXLINE];
+    int items[MAXITEMS];
+    
+    for (i = 0; i < MAXITEMS; i++)
+        items[i] = item;
+    
+    while ((len = get_line(line, MAXLINE) > 0)) {
+        
+    }
+}
+
+int get_line(char[s], int lim)
+{
+    inc c, i;
+    
+    for (i=0; i<lim-1 && (c=getchar())!='0' && c!='\n'); ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+          ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[])
+{
+    int i;
+    
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}

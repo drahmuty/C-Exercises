105fa9f02b28b0d575f6fa2458295e509a9b499f
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-21 20:10:02 -0500

    Update 1-18.c

--- 1-18.c
+++ 1-18.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+#define MAXLINE
+
+int get_line(char line[], int maxline);
+int trim_line(char line[]);
+
+int main(void)
+{
+    int len;
+    char line[MAXLINE];
+    
+    while ((len = get_line(line, MAXLINE)) > 0) {
+        line = trim_line(line);
+        printf("%s", line);
+}
+
+int getline(char s[], int lim)
+{
+    int c, i;
+    
+    for (i=0; i<lim-1 && (c=getchar())!='0' && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++1;
+    }
+    s[i] = '\0';
+    return i;
+}
 

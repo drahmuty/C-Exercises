e760ca891e1ae9c85c0b01ce17c94f16fad5edf8
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-08-31 15:17:52 -0500

    Update 2-03b.c

--- 2-03b.c
+++ 2-03b.c
@@ -17,8 +17,7 @@ void squeeze_mod(char s1[], char s2[])
 {
     int i, j, k;
     
-    i = 0;
-    for (i = 0; s1[i] != '\0'; i++) {
+    for (i = k = 0; s1[i] != '\0'; i++) {
         for (j = 0; s2[j] != '\0'; j++) {
             if (s1[i] == s2[j])
                 i++;

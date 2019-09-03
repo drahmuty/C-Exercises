9586aa54b94d9a6ba9c5cffe3d0ad8efc5429a53
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-02 21:25:22 -0500

    Update 2-05.c

--- 2-05.c
+++ 2-05.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int any(char s1[], char s2[]);
+
+int main(void)
+{
+    char a[] = "this is a test";
+    char b[] = "apple";
+    printf("%d\n", any(a[], b[]));
+}
+
+int any(char s1[], char s2[])
+{
+    int i, j;
+    
+    for (i = 0; s1[i] != '\0'; i++)
+        for (j = 0; s2[j] != '\0'; j++)
+            if (s1[i] == s2[j])
+                return i;
+    return -1;
+}

4a328fd3d7b46f7dcc3be2df12fece1c632cacef
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-03 18:50:11 -0500

    Update 2-07.c

--- 2-07.c
+++ 2-07.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+unsigned invert(unsigned x, int p, int n)
+{
+    unsigned y, z;
+    y = ~x;
+    z = ~(~0 << n) << (p + 1 - n);
+    y = y & z;
+    x = x & ~z;
+    return x | y;
+}
+
+int main(void)
+{
+    unsigned x;
+    printf("%u\n", invert(256, 4, 3));
+}

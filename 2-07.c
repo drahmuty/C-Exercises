0fdbb2443d3d0006716cae82d6fd262b89ea8ee6
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-03 18:58:44 -0500

    Update 2-07.c

--- 2-07.c
+++ 2-07.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 unsigned invert(unsigned x, int p, int n)
-{
+{   
     unsigned y, z;
+    
     y = ~x;
     z = ~(~0 << n) << (p + 1 - n);
     y = y & z;
@@ -15,3 +16,12 @@ int main(void)
     unsigned x;
     printf("%u\n", invert(256, 4, 3));
 }
+
+
+
+
+// Answer from solution manual. Same functionality as my anser, just more elegant.
+// unsigned invert(unsigned x, int p, int n)
+// {
+//     return x ^ (~(~0 << n) << (p + 1 - n));
+// }

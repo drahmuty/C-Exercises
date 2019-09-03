dbab201202e6700644d45c6c6877696d0dcd2108
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-02 22:58:19 -0500

    Update 2-06.c

--- 2-06.c
+++ 2-06.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int setbits(unsigned x, int p, int n, unsigned y);
+
+int main(void)
+{
+    
+}
+
+int setbits(unsigned x, int p, int n, unsigned y)
+{    
+    unsigned z;
+    
+    z = y >> n;
+    z = z << n;
+    z = ~z;
+    z = y & z;
+    z = z << (p - n);
+    
+    
+}

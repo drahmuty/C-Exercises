ef1f7539092a87c2e50bacb4b6056efbdaafd75b
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-03 00:04:16 -0500

    Update 2-06.c

--- 2-06.c
+++ 2-06.c
@@ -4,14 +4,14 @@ unsigned setbits(unsigned x, int p, int n, unsigned y);
 
 int main(void)
 {
-    printf("%u\n", setbits(0, 7, 2, 65535));
+    printf("%u\n", setbits(1, 2, 2, 10));
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y)
 {    
     unsigned z;
 
-    y = (y & ~(~0 << n)) << (p - n);
-    z = ~(~(~0 << n) << (p - n));
+    y = (y & ~(~0 << n)) << (p + 1 - n);
+    z = ~(~(~0 << n) << (p + 1 - n));
     return (x & z) | y;
 }

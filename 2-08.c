19ae9ff437260335f39eb52779299988a7a4f384
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-03 19:14:19 -0500

    Update 2-08.c

--- 2-08.c
+++ 2-08.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+#define MAXP 15
+
+unsigned rightrot(unsigned x, int n)
+{
+    unsigned a, b;
+    
+    a = x >> n;
+    b = x << (MAXP + 1 - n);
+    return a | b;
+}
+
+int main(void)
+{
+    int x = 1;
+    printf("%d\n", rightrot(x, 8));
+}

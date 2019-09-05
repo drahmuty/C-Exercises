c4a8967726b5ffda6367783a8a32e7d6ccd0730f
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-04 20:56:54 -0500

    Update 2-10.c

--- 2-10.c
+++ 2-10.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int lower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return c + 'a' - 'A';
+    else
+        return c;
+}
+
+int lower_mod(int c)
+{
+    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
+
+int main(void)
+{
+    int c = 'B';
+    printf("%d\n", lower(c));
+    printf("%d\n", lower_mod(c));
+}

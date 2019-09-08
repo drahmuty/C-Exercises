bda0868e99f2aea6bc10b89789cb37edf9d00bc8
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-09-08 15:06:08 -0500

    Update 3-05.c

--- 3-05.c
+++ 3-05.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <string.h>
 
+#define abs(x) ((x < 0) ? -(x) : (x))
+
+void reverse(char s[]);
+void itob(int n, char s[], int b);
+
+int main(void)
+{
+    int n, b;
+    char s[1000];
+    
+    b = 16;
+    for (n = 0; n < 1000; n++) {
+        itob(n, s, b);
+        printf("int: %d  base: %d  new: %s\n", n, b, s);
+    }
+
+    return 0;
+}
+
+void reverse(char s[])
+{
+    int c, i, j;
+
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void itob(int n, char s[], int b)
+{
+    int i, sign, m;
+    
+    sign = n;
+    i = 0;
+    do {
+        m = abs(n % b);
+        m = (m > 9) ? (m + 7) : m;      // Skip ASCII symbols between integers and letters
+        s[i++] = m + '0';
+    } while ((n /= b) != 0);
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}

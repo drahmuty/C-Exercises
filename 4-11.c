905b95c6c9e3c1fc8393d1134df537a0bbc35788
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-03 20:27:43 -0500

    Update 4-11.c

--- 4-11.c
+++ 4-11.c
@@ -6,9 +6,14 @@ int getch(void);
 int getop(char s[])
 {
     int i, c;
-    static int last;
-    
-    c = (last) ? last : getch();
+    static int last = 0;
+        
+    if (last == 0)
+        c = getch();
+    else {
+        c = last;
+        last = 0;
+    }
     while ((s[0] = c == ' ' || c == '\t')
         ;
     s[1] = '\0';

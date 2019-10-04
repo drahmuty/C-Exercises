8b6b2e29b1985db530f2c3059beaf0c9b2bfae8d
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-03 20:25:42 -0500

    Update 4-11.c

--- 4-11.c
+++ 4-11.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
+#include <ctype.h>
 
+int getch(void);
+
+int getop(char s[])
+{
+    int i, c;
+    static int last;
+    
+    c = (last) ? last : getch();
+    while ((s[0] = c == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        last = c;
+    return NUMBER;
+}

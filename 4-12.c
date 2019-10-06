ece7ecd7e986f8c7ef5acf87666d9c5e9f1e470d
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-06 12:11:01 -0500

    Update 4-12.c

--- 4-12.c
+++ 4-12.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
 
+void itoarec(int n, char s[]);
+
+int main(void)
+{
+    int a;
+    char s[100];
+
+    a = 256;
+    itoarec(a, s);
+    printf("%s\n", s);
+    return 0;
+}
+
+void itoarec(int n, char s[])
+{
+    static int i = 0;
+    
+    if (n < 0) {
+        n = -n;
+        s[i++] = '-';
+    }
+    if ((n / 10) > 0)
+        itoarec((n / 10), s);
+    s[i++] = n % 10 + '0';
+    s[i] = '\0';
+}

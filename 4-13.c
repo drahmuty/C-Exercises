2444acadb227256c32f53d95f16258fa6ac059f9
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-06 13:19:39 -0500

    Update 4-13.c

--- 4-13.c
+++ 4-13.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
 
+void reverse_rec(char s[]);
+
+int main(void)
+{
+    char a[] = "node";
+    reverse_rec(a);
+    printf("%s\n", a);
+}
+
+void reverse_rec(char s[])
+{
+    static int i = 0;
+    static int j = 1;
+    int c, k;
+    
+    k = strlen(s) - j++;
+    if (i < k) {
+        c = s[i];
+        s[i++] = s[k];
+        s[k] = c;
+        reverse_rec(s);
+    }
+}

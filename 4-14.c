4fe188707160a4744cd4d6d1a93abc0ec6f35f21
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-06 21:19:36 -0500

    Update 4-14.c

--- 4-14.c
+++ 4-14.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+#define swap(t,x,y) {   t temp = x; \
+                        x = y; \
+                        y = temp; }
+
+int main(void)
+{
+    int x, y;
+    
+    x = 1, y = 2;
+    printf("x = %d, y = %d\n", x, y);
+    swap(int, x, y);
+    printf("x = %d, y = %d\n", x, y);
+}

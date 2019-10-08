c4d7008f4eac59ca0f11323dc85bf6e738d4cf92
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-07 22:10:43 -0500

    Update 5-02.c

--- 5-02.c
+++ 5-02.c
@@ -1,1 +1,68 @@
+#include <stdio.h>
+#include <ctype.h>
 
+#define BUFSIZE 100
+#define SIZE 10
+
+int getch(void);
+void ungetch(int);
+double getfloat(double *);
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main(void)
+{
+    int n;
+    double array[SIZE];
+    
+    for (n = 0; n < SIZE && getfloat(&array[n]) != EOF; n++)
+        ;
+    n = 0;
+    while (n < SIZE) {
+        printf("%d: %f\n", n, array[n]);
+        n++;
+    }
+    return 0;
+}
+
+double getfloat(double *pn)
+{
+    double val, power;
+    int c, sign;
+    
+    while (isspace(c = getch()))
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.') {
+        ungetch(c);
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+        c = getch();
+    for (*pn = 0.0; isdigit(c); c = getch())
+        *pn = 10.0 * *pn + (c - '0');
+    if (c == '.')
+        c = getch();
+    for (power = 1.0; isdigit(c); c = getch()) {
+        *pn = 10.0 * *pn + (c - '0');
+        power *= 10;
+    }
+    *pn = sign * *pn / power;
+    if (c != EOF)
+        ungetch(c);
+    return c;
+}
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters");
+    else
+        buf[bufp++] = c;
+}

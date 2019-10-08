3d27ecd78e11ea17f1b6ff078be0e5d6d15d1bf8
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-07 21:17:59 -0500

    Create 5-01.c

---
+++ 5-01.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFSIZE 100
+
+int getch(void);
+void ungetch(int);
+int getint(int *);
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main(void)
+{
+    int n, array[10], getint(int *);
+
+    for (n = 0; n < 10 && getint(&array[n]) != EOF; n++)
+        ;
+    for (n = 0; n < 10; n++)
+        printf("%d: %d\n", n, array[n]);
+    return 0;
+}
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters");
+    else
+        buf[bufp++] = c;
+}
+
+int getint(int *pn)
+{
+    int c, sign;
+
+    while (isspace(c = getch()))
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+        c = getch();
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c);
+    return c;
+}

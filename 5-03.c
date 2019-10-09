00db0ad2dcdefc7b627ba8790028406a2088f964
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-08 21:33:57 -0500

    Update 5-03.c

--- 5-03.c
+++ 5-03.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+void strcatp(char *, char *);
+
+int main(void)
+{
+    char s[] = "Hello";
+    char t[] = ", world!";
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+    strcatp(s, t);
+    printf("s: %s\n", s);
+    return 0;
+}
+
+void strcatp(char *s, char *t)
+{
+    while (*s++)
+        ;
+    *s--;
+    while ((*s++ = *t++))
+        ;
+}

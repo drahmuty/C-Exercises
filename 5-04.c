90c9e2f04e47d785e06ce87059131118d52b1a4d
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-08 22:23:41 -0500

    Update 5-04.c

--- 5-04.c
+++ 5-04.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 
+int strendp(char *, char *);
+
 int maint(void)
-{}
+{
+    int result;
+    char s[] = "What is your favorite color?";
+    char t[] = "color?";
+    
+    result = strendp(s, t);
+    printf("%d\n", result);
+    return 0;
+}
+
+int strendp(char *s, char *t)
+{
+    char *tp = t;
+
+    while (*s)
+        *s++;
+    while (*t)
+        *t++;
+    while (*s == *t) {
+        *s--;
+        *t--;
+    }
+    if (t >= tp)
+        return 0;
+    else
+        return 1;
+}

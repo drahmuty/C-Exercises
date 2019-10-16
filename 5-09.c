c2d5066e62bf4bfb69d29fe077ecf0507090cc45
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-15 21:52:00 -0500

    Update 5-09.c

--- 5-09.c
+++ 5-09.c
@@ -7,43 +7,30 @@ static char daytab[2][13] = {
 
 int day_of_year(int year, int month, int day)
 {
-    int i, leap;
+    int leap;
+    char *y, *m;
     
-    leap = (year%4 == 0 && year%100 == 0) || year%400 == 0;
-    if (year < 1) {
-        printf("invalid year\n");
-        return -1;
-    }
-    if (month < 1 || month > 12) {
-        printf("invalid month\n");
-        return -1;
-    }
-    if (day < 1 || day > daytab[leap][month]) {
-        printf("invalid day\n");
-        return -1;
-    }
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+    if ((year%4 == 0 && year%100 != 0) || year%400 == 0)
+        y = daytab+1;
+    else
+        y = daytab;
+    for (m = y; month > 0; month--, m++)
+        day += *m;
     return day;
 }
 
 void month_day(int year, int yearday, int *pmonth, int *pday)
 {
-    int i, leap, maxyd;
+    int leap, month;
+    char *y, *m;
     
-    leap = (year%4 == 0 && year%100 == 0) || year%400 == 0;
-    maxyd = (leap) ? 366 : 365;
-    if (year < 1) {
-        printf("invalid year\n");
-        return;
-    }
-    if (yearday < 1 || yearday > maxyd) {
-        printf("inavlid yearday\n");
-        return;
-    }
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
-    *pmonth = i;
+    if ((year%4 == 0 && year%100 != 0) || year%400 == 0)
+        y = daytab+1;
+    else
+        y = daytab;
+    for (m = y+1; yearday > *m; m++)
+        yearday -= *m;
+    *pmonth = m - y;
     *pday = yearday;
 }
 
@@ -52,14 +39,14 @@ int main(void)
     int y, m, d, yd, pm, pd;
     
     y = 1991;
-    m = 2;
-    d = 16;
+    m = 10;
+    d = 1;
     yd = day_of_year(y, m, d);
     printf("yearday = %d\n", yd);
     pm = 4;
     pd = 2;
     printf("month = %d, day = %d\n", pm, pd);
-    month_day(2001, 365, &pm, &pd);
+    month_day(2000, yd, &pm, &pd);
     printf("month = %d, day = %d\n", pm, pd);
     return 0;
 }

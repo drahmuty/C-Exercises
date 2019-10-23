c65dd152c10e12c3d733849eb13b27a3620574c1
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-22 20:50:11 -0500

    Update 1-20.c

--- 1-20.c
+++ 1-20.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-#define TABSTOP 4
+#define TAB 4
 
 int main(void)
 {
+    int c, i, sp;
+    
+    i = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            putchar(c);
+            i = 0;
+        }
+        else if (c == '\t') {
+            sp = TAB - (i % TAB);
+            while (sp-- > 0)
+                putchar(' ');
+            i = 0;
+        }
+        else {
+            putchar(c);
+            i++;
+        }
+    }
+    return 0;
 }

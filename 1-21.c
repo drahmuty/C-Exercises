0b5aa2fd3e7198bb6df066ed46fecec533b97c75
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-10-27 20:27:22 -0500

    Update 1-21.c

--- 1-21.c
+++ 1-21.c
@@ -1,1 +1,38 @@
+// entab program
 
+#include <stdio.h>
+
+#define TAB 4
+
+int main(void)
+{
+    int c, pos, ns, nt;
+    
+    pos = 1;
+    nt = ns = 0;
+    while ((c = getchar()) != EOF) {
+        while (c == ' ') {
+            if (pos % TAB == 0) {
+                nt++;
+                ns = 0;
+            } else
+                ns++;
+            pos++;
+            c = getchar();
+        }
+        for ( ; nt > 0; nt--)
+            putchar('\t');
+        for ( ; ns > 0; ns--)
+            putchar(' ');
+        if (c == '\n') {
+            putchar(c);
+            pos = 1;
+        } else if (c == '\t') {
+            putchar(c);
+            pos += TAB - (pos - 1) % TAB;
+        } else {
+            putchar(c);
+            pos++;
+        }
+    }
+}

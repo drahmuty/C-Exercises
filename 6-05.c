78d2bd71c0fb085f3ae4a3b03f60291cf8370e4f
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-11-04 20:40:55 -0600

    Update 6-05.c

--- 6-05.c
+++ 6-05.c
@@ -22,28 +22,59 @@ int main(void)
 {    
     char key[] = "key1";
     char val[] = "value1";
+    char k2[] = "hello";
+    char v2[] = "world";
+    char k3[] = "david";
+    char v3[] = "ahmud";
     install(key, val);
-    printf("%s\n", lookup(key)->defn);
-    undef(key);
-    printf("%s\n", lookup(key)->defn);
+    install(k2, v2);
+    install(k3, v3);
+    printf("%s\n", lookup(k2)->defn);
+    undef(k2);
+    printf("%s\n", lookup(k2)->defn);
     return 0;
 }
 
 void undef(char *s)
 {
     struct nlist *np, *xp;
-    unsigned hashval;
+    unsigned h;
     
     hashval = hash(s);
-    for (xp = np = hashtab[hashval]; np != NULL; xp = np, np = np->next) {
+    for (xp = np = hashtab[h]; np != NULL; xp = np, np = np->next) {
         if (strcmp(s, np->name) == 0) {
             xp = np->next;
-            hashtab[hashval] = xp;
-            np = NULL;
+            free((void *) np->name);
+            free((void *) np->defn);
+            free((void *) np);
             return;
         }
     }
     return;
+}
+
+// Book answer version
+void undef_ans(char *s)
+{
+    int h;
+    struct nlist *prev, *np;
+    
+    prev = NULL;
+    h = hash(s);
+    for (np = hashtab[h]; np != NULL; np = np->next) {
+        if (strcmp(s, np->name) == 0)
+            break;
+        prev = np;
+    }
+    if (np != NULL) {
+        if (prev == NULL)
+            hashtab[h] = np->next;
+        else
+            prev->next = np->next;
+        free((void *) np->name);
+        free((void *) np->defn);
+        free((void *) np);
+    }
 }
 
 unsigned hash(char *s)
@@ -59,7 +90,7 @@ struct nlist *lookup(char *s)
 {
     struct nlist *np;
     
-    for (np = hashtab[hash(s)]; np != NULL; np = np-> next)
+    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
         if (strcmp(s, np->name) == 0)
             return np;
     return NULL;

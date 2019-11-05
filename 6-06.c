250b3c6ba485de88312f0d76b8d198fa4ab13554
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-11-04 20:52:26 -0600

    Update 6-06.c

--- 6-06.c
+++ 6-06.c
@@ -1,3 +1,31 @@
-/* 
+/*
+scan entire program (first pass)
+if #define is encountered
+install new key-value pair in hash table
+first word after #define is key
+second word is value
 
+scan entire program (second pass)
+if any word matches a key in the hash table
+replace that word with the key's value
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+    
+    // while getword != EOF
+    // if word == #define
+    // get next word and set it as key
+    // get next word and set it as val
+    // install key-val in hash table
+    
+    // while getword != EOF
+    // if lookup(word)
+    // replace word with val
+    // else write word to output
+    
+    return 0;
+}
